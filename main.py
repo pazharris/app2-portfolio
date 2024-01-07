099a152a57855278fdbde9251f8eced722755bdb
Paul Harris <pazthreed@gmail.com>
2024-01-07 10:11:44 +0000

    Create columns, image and description

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/bw_me.jpg")
+
+with col2:
+    st.title("Paul Harris")
+    content = """
+    I am a freelance Python programmer based in the UK. I am PCEP and PCAP certified
+    and passed the Harvard CS50x and CS50 Python exams. This web page is to showcase
+    some of the skills I have learned.
+    """
+    st.info(content)
