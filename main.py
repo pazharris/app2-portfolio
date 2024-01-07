1b5ba11ce36fc69fe7d0817b9bbd800aaddb0afb
Paul Harris <pazthreed@gmail.com>
2024-01-07 10:30:40 +0000

    A description on the new columns

--- main.py
+++ main.py
@@ -10,7 +10,12 @@ with col2:
     st.title("Paul Harris")
     content = """
     I am a freelance Python programmer based in the UK. I am PCEP and PCAP certified
-    and passed the Harvard CS50x and CS50 Python exams. This web page is to showcase
+    and passed the Harvard CS50x and CS50 Python exams.  This web page is to showcase
     some of the skills I have learned.
     """
     st.info(content)
+
+content2 = """
+Below you can find some of the apps I have built in Python. Feel free to contact me!
+"""
+st.write(content2)
