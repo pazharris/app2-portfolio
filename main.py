96ca383887117df5cd34384be04cc75f235761e2
Paul Harris <pazthreed@gmail.com>
2024-01-09 10:34:41 +0000

    Add app description, image, and links.

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ with col2:
     st.title("Paul Harris")
     content = """
     I am a freelance Python programmer based in the UK. I am PCEP and PCAP certified
-    and passed the Harvard CS50x and CS50 Python course exams. This web page is to showcase
+    and have passed the Harvard CS50x and CS50 Python course exams. This web page is to showcase
     some of the Apps I have built with the skills I have learned.
     """
     st.info(content)
@@ -21,14 +21,20 @@ Below you can find some of the apps I have built in Python. Feel free to contact
 """
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=",")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
